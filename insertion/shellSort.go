5d9b3ba1ca89c85b52912b486f0e1991a85bd356
Rok Nikolič <rok.nikolic@gmail.com>
2024-03-10 15:05:08 +0100

    shell sort finished

--- insertion/shellSort.go
+++ insertion/shellSort.go
@@ -1,14 +1,19 @@
 package insertionsorts
 
+// Improvement on insertion sort, using a decreasing gap to move outer elements fast
+
 func ShellSort(dataSlice []int) []int {
-	for i := 1; i < len(dataSlice); i++ {
-		currentValue := dataSlice[i]
-		dataSlice = append(dataSlice[:i], dataSlice[i+1:]...)
-		insertionAt := i
-		for insertionAt > 0 && dataSlice[insertionAt-1] > currentValue {
-			insertionAt -= 1
+	gaps := []int{701, 301, 132, 57, 23, 10, 4, 1} // Ciura gap sequence
+	for _, gap := range gaps {
+		for i := gap; i < len(dataSlice); i++ {
+			currentValue := dataSlice[i]
+			dataSlice = append(dataSlice[:i], dataSlice[i+1:]...)
+			insertionAt := i
+			for insertionAt >= gap && dataSlice[insertionAt-gap] > currentValue {
+				insertionAt -= gap
+			}
+			dataSlice = insert(currentValue, insertionAt, dataSlice)
 		}
-		dataSlice = insert(currentValue, insertionAt, dataSlice)
 	}
 	return dataSlice
 }
