5f8ca6d30d6e99d59baafede3ec0ec673bedaa58
Rok Nikolič <rok.nikolic@gmail.com>
2024-03-24 18:57:21 +0100

    odd-even done

--- sort/oddEvenSort.go
+++ sort/oddEvenSort.go
@@ -1,1 +1,25 @@
 package sort
+
+// Improvement to Bubble sort, comparing odd and even elements, can be parallelized
+
+func OddEvenSort(dataSlice []int) []int {
+	sorted := false
+	for !sorted {
+		sorted = true
+		// Odd
+		for i := 1; i < len(dataSlice)-1; i += 2 {
+			if dataSlice[i] > dataSlice[i+1] {
+				dataSlice[i], dataSlice[i+1] = dataSlice[i+1], dataSlice[i]
+				sorted = false
+			}
+		}
+		// Even
+		for i := 0; i < len(dataSlice)-1; i += 2 {
+			if dataSlice[i] > dataSlice[i+1] {
+				dataSlice[i], dataSlice[i+1] = dataSlice[i+1], dataSlice[i]
+				sorted = false
+			}
+		}
+	}
+	return dataSlice
+}
